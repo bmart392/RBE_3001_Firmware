2d4c6a148978518d8b5d0bad113e06e39e58543a
thatrobotguy <andrewtheschueler@gmail.com>
2018-02-24 14:44:54 -0500

    Gripper Server added.

---
+++ src/coms/GripperServer.cpp
@@ -0,0 +1,52 @@
+/*
+ * GripperServer.cpp
+ *
+ *  Created on: Feb 24, 2018
+ *      Author: arschueler
+ */
+
+#include "GripperServer.h"
+
+void GripperServer::event(float * buffer)
+{
+
+	// we will be using the same memory area in which the incoming packet was stored,
+	// however, we need to perform a type cast first (for convenience).
+	uint8_t* buff = (uint8_t*) buffer;
+
+	/*
+	*	Gripper : The first element in the 15x1 array is either a 1 or a 0.
+	*	If the gripper receives a 1, the gripper opens. If the gripper
+	*	receives a 0, the gripper closes.
+	*
+	*/
+	wantedPosition = buffer[0];
+
+	if (wantedPosition == 1)
+	{
+		for(int i=0; i<100; i++)
+		{
+			myServo = i/100.0;
+			wait(0.01);
+		}
+		currentPosition = wantedPosition;
+	}
+	else // if (wantedPosition == 0)
+	{
+		for(int i=100; i>0; i--)
+		{
+			myServo = i/100.0;
+			wait(0.01);
+		}
+		currentPosition = wantedPosition;
+	}
+
+	// re-initialize the packet to all zeros
+	for (int i = 4; i < 64; i++)
+		buff[i] = 0;
+
+	// Send back the position
+	buffer[0] = currentPosition;
+
+
+}
---
+++ src/coms/GripperServer.h
@@ -0,0 +1,41 @@
+/*
+ * GripperServer.h
+ *
+ *  Created on: Feb 24, 2018
+ *      Author: arschueler
+ */
+
+#ifndef SRC_COMS_GRIPPERSERVER_H_
+#define SRC_COMS_GRIPPERSERVER_H_
+
+#include <PacketEvent.h>
+#include "Servo.h"
+#include <cmath>        // std::abs
+
+#define GRIPPPER_SERVER_ID 13
+
+class GripperServer : public PacketEventAbstract
+{
+private:
+
+// This is the gripper servo instantiation that goes into the server constructor.
+// Servo myServo;
+int currentPosition = 0; // Initialize to the closed position.
+int wantedPosition = 0;
+
+public:
+
+	GripperServer () : PacketEventAbstract( GRIPPPER_SERVER_ID )
+	{
+		myServo(PB_10, 40);
+		myServo = currentPosition;
+	}
+
+	  //User function to be called when a packet comes in
+	  // Buffer contains data from the packet coming in at the start of the function
+	  // User data is written into the buffer to send it back
+	  void event(float * buffer);
+
+};
+
+#endif /* SRC_COMS_GRIPPERSERVER_H_ */
